e85254617b0d87b25234fd327e909ee9c1365df7
German Baena <germanb66@gmail.com>
2020-11-14 22:05:17 -0500

    _header.scss y _menu.scss completos en version movil y desktop

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,32 @@
-console.log('Hola, Bienvenidos a CompuAndina S.A.S.')
+
+const   header = document.getElementById('main-header'),
+        burguerButton = document.querySelector('.burguer-button'),
+        burguerLine = document.querySelector('.burguer-button__line'),
+        nav = document.getElementById('main-nav'),
+        mql = matchMedia('(min-width: 1024px)')
+
+
+// Hamburger button animation
+burguerButton.addEventListener('click', () => {
+    burguerLine.classList.toggle('cruz')
+    nav.classList.toggle('main-nav__move')
+})
+
+
+const changeIcons = () => {
+    if (mql.matches) {
+        document.getElementById("checkin").src = "img/icons/checkin-desk.svg"
+        document.getElementById("contact").src = "img/icons/contact-desk.svg"
+    }
+
+    else {
+        document.getElementById("checkin").src = "img/icons/checkin-mobl.svg"
+        document.getElementById("contact").src = "img/icons/contact-mobl.svg"
+    }
+}
+
+changeIcons()
+
+mql.addEventListener('change', changeIcons)
+
 
