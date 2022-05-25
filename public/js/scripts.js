f47681a424b42473ccdcc0e592b97302ad5d6ab6
German Baena <germanb66@gmail.com>
2022-05-24 20:04:24 -0500

    "button" está funcionando correctamente en "normal", "hover" y "active"

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -270,7 +270,7 @@ const changeState = (condicion, elemento) => {
 
 	if (condicion) {
 		message.innerText = ''
-		icon.style.color = '#0075ff'
+		icon.style.color = '#0099ff'
 		message.style.marginBottom = 0
 		checkInputs[elemento.name] = true
 		label.style.marginBottom = '8px'
